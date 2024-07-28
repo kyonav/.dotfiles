f893b1dedba1c860c66dbb6dd2f7647bd878e238
kyonav <sobbing_rat@protonmail.com>
2024-07-27 23:48:30 -0300

    Added support for js, ts, html, css and json

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "javascript", "typescript", "html"},
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "javascript", "typescript", "html", "css", ".json" }
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
