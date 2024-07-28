d0d3b379f5d11ad1dacc02b93cba2b69a404dc7c
kyonav <sobbing_rat@protonmail.com>
2024-07-27 23:49:44 -0300

    Added prettier and null-ls plugins

--- nvim/lua/slvt/packer.lua
+++ nvim/lua/slvt/packer.lua
@@ -43,6 +43,8 @@ return require('packer').startup(function(use)
 		  -- Snippets
 		  {'L3MON4D3/LuaSnip', dependencies = {"rafamadriz/friendly-snippets"}},
 		  {'rafamadriz/friendly-snippets'},
+          {'jose-elias-alvarez/null-ls.nvim'},
+          {'MunifTanjim/prettier.nvim'}
 	  }
   }
 
