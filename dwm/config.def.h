9b387902429a5bb1ec36e380749eca5c6eeaf4ef
kyonav <gore68plus1@protonmail.com>
2025-02-23 02:17:56 -0300

    program startup tweaks

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -31,8 +31,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Firefox",  NULL,       NULL,       3,            0,           -1 },
-    { "discord",  NULL,       NULL,       1,            0,           -1 },
+	{ "Firefox",      NULL,       NULL,       2,            0,           -1 },
+    { "discord",      NULL,       NULL,       3,            0,           -1 },
+    { "st-256color",  NULL,       NULL,       0 >> 1,       0,           -1 }, 
 };
 
 /* layout(s) */
