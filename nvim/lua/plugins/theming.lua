6c323f7b6e534e3b22cf595d7e8238dd1dae7a13
kyonav <tokyonav@proton.me>
2025-05-01 07:25:56 -0300

    Added: and applied boo-colorscheme theme

--- nvim/lua/plugins/theming.lua
+++ nvim/lua/plugins/theming.lua
@@ -1,24 +1,44 @@
-    function ApplyTheme(color)
-        color = color or "oxocarbon"
+function ApplyTheme(color)
+    color = color or "boo"
     vim.cmd.colorscheme(color)
+
+    if vim.fn.has("termguicolors") then
+        vim.opt.termguicolors = true
+    end
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
 
 end
 
 return {
     {
-        "nyoom-engineering/oxocarbon.nvim",
-        name = "oxocarbon",
+        'rockerBOO/boo-colorscheme-nvim',
+        name = "boo",
         lazy = false,
         priority = 1000,
 
-        config = function ()
-            ApplyTheme("oxocarbon")
+        config = function()
+            require("boo-colorscheme").use({
+                italic = false, -- toggle italics
+                theme = "boo",
+            })
 
-        end
+            ApplyTheme("boo");
+       end
     },
+--    {
+--        'nyoom-engineering/oxocarbon.nvim',
+--        name = "oxocarbon",
+--        lazy = false,
+--        priority = 1000,
+--
+--        config = function ()
+--            ApplyTheme("oxocarbon")
+--
+--        end
+--    },
 --    {
 --        'catppuccin/nvim',
 --        name = "catppuccin",
