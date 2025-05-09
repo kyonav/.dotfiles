870ad82b60c77ff0b2f8b78eb16de134cf771a85
kyonav <tokyonav@proton.me>
2025-05-09 12:47:24 -0300

    qol updates 2

--- nvim/lua/plugins/theming.lua
+++ nvim/lua/plugins/theming.lua
@@ -1,5 +1,5 @@
 function ApplyTheme(color)
-    color = color or "boo"
+    color = color or "catppuccin"
     vim.cmd.colorscheme(color)
 
     if vim.fn.has("termguicolors") then
@@ -9,25 +9,35 @@ function ApplyTheme(color)
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
-
 end
 
 return {
-    {
-        'rockerBOO/boo-colorscheme-nvim',
-        name = "boo",
-        lazy = false,
-        priority = 1000,
-
-        config = function()
-            require("boo-colorscheme").use({
-                italic = false, -- toggle italics
-                theme = "boo",
-            })
-
-            ApplyTheme("boo");
-       end
-    },
+--    {
+--        "hachy/eva01.vim",
+--        name = eva01,
+--        lazy = false,
+--        priority = 1000,
+--        config = function()
+--            ApplyTheme("eva01");
+--            -- or
+--            -- vim.cmd.colorscheme "eva01-LCL"
+--        end,
+--    },
+--    {
+--        'rockerBOO/boo-colorscheme-nvim',
+--        name = "boo",
+--        lazy = false,
+--        priority = 1000,
+--
+--        config = function()
+--            require("boo-colorscheme").use({
+--                italic = false, -- toggle italics
+--                theme = "boo",
+--            })
+--
+--            ApplyTheme("boo");
+--       end
+--    },
 --    {
 --        'nyoom-engineering/oxocarbon.nvim',
 --        name = "oxocarbon",
@@ -39,18 +49,28 @@ return {
 --
 --        end
 --    },
---    {
---        'catppuccin/nvim',
---        name = "catppuccin",
---        lazy = false,
---        priority = 1000,
---
---    },
+    {
+        'catppuccin/nvim',
+        name = "catppuccin",
+        lazy = false,
+        priority = 1000,
+
+        config = function ()
+            ApplyTheme("catppuccin")
+
+        end
+
+    },
 --    {
 --        'olivercederborg/poimandres.nvim',
 --        name = "poimandres",
 --        lazy = false,
 --        priority = 1000,
+--
+--        config = function ()
+--            ApplyTheme("poimandres")
+--
+--        end
 --    },
 --    {
 --        "folke/tokyonight.nvim",
