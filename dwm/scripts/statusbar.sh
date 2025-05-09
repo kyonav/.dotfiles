d2c4cf3b757f8b35ee5996a95c825469af6367a9
kyonav <tokyonav@proton.me>
2025-05-09 09:51:02 -0300

    qol updates

--- dwm/scripts/statusbar.sh
+++ dwm/scripts/statusbar.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
-dte() # adding date overlay
-{    
-    dte="$(date +"%a, %b %d ] [ %l:%M:%S %p")"
-    echo -e "$dte"
-}
-
-while true; do # outputing and updating the overlays
-    xsetroot -name " [ $(dte) ] "
-    sleep 1s # update every half a second
-
-done &
+#dte() # adding date overlay
+#{    
+#    dte="$(date +"%a, %b %d ] [%l:%M:%S %p")"
+#    echo -e "$dte"
+#}
+#
+#while true; do # outputing and updating the overlays
+#    xsetroot -name " [ $(dte) ] "
+#    sleep 1s # update every half a second
+#
+#done &
