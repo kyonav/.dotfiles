d36f8664191f53071c7b92e4533b98b5e8487235
kyonav <tokyonav@proton.me>
2025-05-17 22:58:03 -0300

    switching to hyprland

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -12,6 +12,5 @@ return
             },
 
             signature = { enabled = true },
-
         },
     }
--- nvim/lua/plugins/roslyn.lua
+++ nvim/lua/plugins/roslyn.lua
@@ -26,6 +26,15 @@ return {
                 cmd = cmd,
                 handlers = require("rzls.roslyn_handlers"),
                 settings = {
+                    ["csharp|background_analysis"] = {
+                        dotnet_analyzer_diagnostics_scope = fullSolution,
+                        dotnet_compiler_diagnostics_scope = fullSolution,
+                    },
+                    ["csharp|completion"] = {
+                        dotnet_provide_regex_completions = true,
+                        dotnet_show_completion_items_from_unimported_namespaces = true,
+                        dotnet_show_name_completion_suggestions = true,
+                    },
                     ["csharp|inlay_hints"] = {
                         csharp_enable_inlay_hints_for_implicit_object_creation = true,
                         csharp_enable_inlay_hints_for_implicit_variable_types = true,
@@ -43,6 +52,13 @@ return {
                     },
                     ["csharp|code_lens"] = {
                         dotnet_enable_references_code_lens = true,
+                        dotnet_enable_tests_code_lens = false,
+                    },
+                    ["csharp|symbol_search"] = {
+                        dotnet_search_reference_assemblies = true;
+                    },
+                    ["csharp|formatting"] = {
+                        dotnet_organize_imports_on_format = false;
                     },
                 },
             }
